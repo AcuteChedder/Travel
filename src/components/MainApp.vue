ea5a7b993ce93408d9af0ab3ca97ac217762ac56
AcuteChedder <nubik8653@gmail.com>
2025-03-28 23:46:17 +0300

    END

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -411,7 +411,13 @@ export default {
                     </div>
 
                     <div class="your-tour">
-                        
+                        <h1 class="your-title">Crafting Your <br> Perfect Travel Experience</h1>
+                        <button class="your-btn">
+                            <div class="btn-text">
+                                <p class="your-text">Browse <br> All destinations</p> 
+                                <img src="../assets/main/Link.png" alt="" class="your-link">
+                            </div>
+                        </button>
                     </div>
 
                 </div>
@@ -419,6 +425,59 @@ export default {
 
         </div>
     </section>
+
+    <footer>
+        <div class="footer-main">
+            <div class="container">
+                <div class="footer-content">
+                    <div class="support">
+                        <img class="footer-logo" src="../assets/logo.png" alt="">
+
+                        <div class="footer-section">
+                            <img class="icon" src="../assets/footer/location.png" alt="">
+                            <p class="address">4517 Washington Ave. Manchester, Kentucky <span class="address-span">39495</span></p>
+                        </div>
+
+                        <div class="footer-section">
+                            <img class="icon" src="../assets/footer/clock.png" alt="">
+                            <p class="time">Hours: 8:00 - 17:00, Mon - Sat</p>
+                        </div>
+
+                        <div class="footer-section">
+                            <img class="icon" src="../assets/footer/mail.png" alt="">
+                            <p class="mail">support@travila.com</p>
+                        </div>
+                        
+                    </div>
+                    
+                    <div class="footer-services">
+                        <ul class="services-list">
+                            <h3 class="footer-services-title">Services</h3>
+                            <li class="footer-service">Tour Guide</li>
+                            <li class="footer-service">Tour Booking</li>
+                            <li class="footer-service">Hotel Booking</li>
+                        </ul>
+
+                        <ul class="footer-services-list-2">
+                            <li class="footer-service">Ticket Booking</li>
+                            <li class="footer-service">Rental Services</li>
+                            <li class="footer-service">Travel plane</li>
+                        </ul>
+                    </div>
+
+                    <div class="newsletter-section">
+                        <h3 class="newsletter-title">Subscribe For Newsletter</h3>
+                        <form action="">
+                            <input type="email" placeholder="Enter your email" class="form-input">
+                            <button class="form-btn">Subscribe</button>
+                        </form>
+                        <p class="postscript">No ads. No trails. No commitments</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </footer>
 </template>
 
 <style scoped> 
@@ -922,6 +981,111 @@ a, button {
         display: flex;
         gap: 16px;
         margin-top: 25px;
+    }
+
+    .your-title {
+        font-size: 24px;
+        font-weight: 900;
+    }
+
+    .your-btn {
+        padding: 12px 26px;
+        background-color: #000;
+        border: none;
+        color: #fff;
+        font-size: 15px;
+        margin-top: 35px;
+        border-radius: 16px;
+    }
+
+    .btn-text {
+        display: flex;
+        gap: 50px;
+        text-align: left;
+    }
+
+    .your-link {
+        width: 36px;
+        height: 36px;
+        margin-top: 3px;
+    }
+
+
+    .footer-main {
+        background-image: url(../assets/footer/Footer.png);
+        background-repeat: no-repeat;
+        background-position: center;
+        min-height: auto;
+        background-size: cover;
+        background-position: center bottom;
+        padding: 180px 0;
+        font-size: 16px;
+    }
+
+    .footer-content {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        gap: 60px;
+    }
+
+    .footer-services {
+        display: flex;
+        gap: 100px;
+    }
+
+    .footer-section {
+        display: flex;
+        gap: 5px;
+        margin-bottom: 16px;
+        color: #8E8E8E;
+    }
+    
+    .icon {
+        width: 18px;
+        height: 18px;
+        margin-top: 4px;
+    }
+
+    .footer-logo {
+        margin-bottom: 30px;
+    }
+
+    .footer-services-title {
+        margin-bottom: 20px;
+        font-size: 18px;
+    }
+
+    .footer-service {
+        margin-bottom: 18px;
+        color: #8E8E8E;
+    }
+
+    .footer-services-list-2 {
+        margin-top: 44px;
+    }
+
+    .newsletter-title {
+        font-size: 18px;
+    }
+
+    .form-input {
+        padding: 20px 50px;
+        border-radius: 100px;
+        border: 1px solid #737373;
+    }
+
+    .form-btn {
+        padding: 18px 22px;
+        background-color: #000;
+        color: #fff;
+        border-radius: 100px;
+        border: none;
+    }
+
+    .postscript {
+        color: #737373;
+        font-size: 12px;
+        margin-top: 16px;
     }
 
 </style>
